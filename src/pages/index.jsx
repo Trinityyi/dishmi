ce46a115b0214ad218d239aed88998512e8d9c6c
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-09-26 22:47:18 +0300

    Center button text in index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,7 +33,7 @@ const Home = () => {
           />
           <a
             ref={linkRef}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold w-40 block py-2 px-4 mt-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold w-40 block py-2 px-4 mt-4 rounded focus:outline-none focus:shadow-outline text-center"
             href={`/${pin}`}
           >
             View
